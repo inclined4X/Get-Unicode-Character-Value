4a5896c51938807ef42ee5628d38dccbd99aabdc
inclined4X <jezemiahsam48@gmail.com>
2024-02-02 23:12:19 +0000

    finished with JS

---
+++ script.js
@@ -0,0 +1,10 @@
+function detectChar() {
+  const input = document.getElementById("inputType").value;
+  if (input) {
+    const uniCodeValue = input.charCodeAt(0);
+    const result = `The unicode value of "${input}" is ${uniCodeValue}`;
+    document.getElementById("result").textContent = result;
+  } else {
+    document.getElementById("result").textContent = "Please Enter a Character";
+  }
+}
